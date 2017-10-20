d7fbf89ef391d0a8ee3476d22bf90503171d2608
DarySchaeken <dary.schaeken@student.pxl.be>
2017-10-20 12:09:22 +0200

    Commented out some code
    Triangle bugged me to no end.

--- src/be/pxl/generics/opdracht3/TestWorkingPlace.java
+++ src/be/pxl/generics/opdracht3/TestWorkingPlace.java
@@ -2,9 +2,9 @@ package be.pxl.generics.opdracht3;
 
 public class TestWorkingPlace {
 	public static void main(String[] args) {
-		Car car = new Car();
-		CircularSaw circularSaw = new CircularSaw();
-		Bike bike = new Bike();
+		Car car = new Car(); // Activate when necessary
+		//CircularSaw circularSaw = new CircularSaw(); // Activate when necessary
+		//Bike bike = new Bike(); // Activate when necessary
 		// WorkingPlace<Motorized> test = new WorkingPlace<>(); //Ex 3.1
 		// WorkingPlace<Vehicle> test = new WorkingPlace<>(); //Ex 3.2
 		// WorkingPlace<Bike> test = new WorkingPlace<>(); //Ex 3.3
